d441c5146514491cc4448cda30fd994a92da67e7
Arisemcdc <arisemcdc1@gmail.com>
2020-05-02 22:27:26 +0300

    добавил getUsers()

--- app/src/main/java/com/example/testdb2/MainActivity.kt
+++ app/src/main/java/com/example/testdb2/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
 //val users = arrayListOf<User>()
-val users = MutableLiveData<List<User>>()
+//val users = MutableLiveData<List<User>>()
 class MainActivity : AppCompatActivity() {
     lateinit var userListAdapter: UserListAdapter
     //val users = MutableLiveData<List<User>>()
@@ -18,9 +18,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //usersList.layoutManager =  LinearLayoutManager(this)
+        //usersList.adapter = UserListAdapter(users)
+        val db=Room.databaseBuilder(applicationContext, RoomDB::class.java, "DB").allowMainThreadQueries().build()
+        val users = db.userDao().getUsers()
         usersList.layoutManager =  LinearLayoutManager(this)
         usersList.adapter = UserListAdapter(users)
-        val db=Room.databaseBuilder(applicationContext, RoomDB::class.java, "DB").allowMainThreadQueries().build()
         users.observe(this, Observer {
             userListAdapter = UserListAdapter(it)
            usersList.adapter = userListAdapter
@@ -31,14 +34,17 @@ class MainActivity : AppCompatActivity() {
             user.name = nameeditText.text.toString()
             user.age = ageeditText.text.toString().toInt()
             db.userDao().insert(user)
-            users.value.add(user)
         }
         deleteButton.setOnClickListener {
             val user = User()
+            user.name = nameeditText.text.toString()
+            user.age = ageeditText.text.toString().toInt()
             db.userDao().delete(user)
         }
         updateButton.setOnClickListener {
             val user = User()
+            user.name = nameeditText.text.toString()
+            user.age = ageeditText.text.toString().toInt()
             db.userDao().update(user)
         }
 
